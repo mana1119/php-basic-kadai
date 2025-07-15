26fc5011537c9dca10a6a5931ee8037f644b8659
mana1119 <mana66481@gmail.com>
2025-07-15 23:31:09 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,46 @@
+<?php
+
+class Food {
+    
+    public $name;
+    public $price;
+
+    public function __construct($name, $price) {
+        $this->name = $name;     // 名前を代入
+        $this->price = $price;   // 価格を代入
+    }
+    
+    public function show_price() {
+        echo $this->price;
+    }
+}
+
+
+class Animal {
+    
+    public $name;
+    public $height;
+    public $weight;
+
+    
+    public function __construct($name, $height, $weight) {
+        $this->name = $name;       // 名前を代入
+        $this->height = $height;   // 身長を代入
+        $this->weight = $weight;   // 体重を代入
+    }
+
+    
+    public function show_height() {
+        echo $this->height;
+    }
+}
+
+$banana = new Food("バナナ", 120);
+print_r($banana);     
+
+
+
+$cat = new Animal("タマ", 25, 4.5);
+ print_r($cat);        
+
+?>
